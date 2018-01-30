f1883da98684865640a89242496870725dbe1a00
DELL <DELL@172.30.63.10>
2018-01-30 14:31:49 -0500

    actualizacion

--- Testeos/src/ec/edu/unach/sicoa/vistas/LISTAESTUDIANTEV.java
+++ Testeos/src/ec/edu/unach/sicoa/vistas/LISTAESTUDIANTEV.java
@@ -1,7 +1,20 @@
 
 package ec.edu.unach.sicoa.vistas;
+import java.awt.BorderLayout;
 import javax.swing.*;
 
 public class LISTAESTUDIANTEV extends JInternalFrame{
+
+    public LISTAESTUDIANTEV() {
+        
+        
+        this.setSize(800, 600);
+        this.setLayout(new BorderLayout());
+        this.setClosable(true);
+        
+        
+    }
+    
+    
     
 }
