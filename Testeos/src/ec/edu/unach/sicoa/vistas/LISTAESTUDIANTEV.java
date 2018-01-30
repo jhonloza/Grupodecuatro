27a3afcb24fc3d01c0343eef2a0235a7307977d3
DELL <DELL@172.30.63.10>
2018-01-30 14:58:54 -0500

    actualizacion

--- Testeos/src/ec/edu/unach/sicoa/vistas/LISTAESTUDIANTEV.java
+++ Testeos/src/ec/edu/unach/sicoa/vistas/LISTAESTUDIANTEV.java
@@ -1,20 +1,58 @@
-
 package ec.edu.unach.sicoa.vistas;
+
 import java.awt.BorderLayout;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import ec.edu.unach.sicoa.rnegocio.impl.*;
+import ec.edu.unach.sicoa.rnegocio.dao.*;
+import ec.edu.unach.sicoa.rnegocio.entidades.*;
+import java.util.*;
 
-public class LISTAESTUDIANTEV extends JInternalFrame{
+public class LISTAESTUDIANTEV extends JInternalFrame {
+
+    JLabel titulo;
+    JTable tabla;
+    DefaultTableModel modelo;
 
     public LISTAESTUDIANTEV() {
-        
-        
+
         this.setSize(800, 600);
         this.setLayout(new BorderLayout());
         this.setClosable(true);
-        
-        
+        titulo = new JLabel("LISTADO DE ESTUDIANTES");
+        tabla = new JTable();
+
+        this.add(titulo, BorderLayout.NORTH);
+        this.add(tabla, BorderLayout.CENTER);
+
+        cargarTabla();
+
     }
-    
-    
-    
+
+    public void cargarTabla() {
+        modelo = new DefaultTableModel();
+        modelo.addColumn("codigo");
+
+        modelo.addColumn("NUMERO CEDULA");
+
+        modelo.addColumn("NOMBRE");
+
+        modelo.addColumn("APELLIDO");
+
+        modelo.addColumn("FECHA NACIMIENTO");
+
+        List<Estudiante> lista = new ArrayList<>();
+        try {
+            IEstudiante estDao = new EstudianteImpl();
+            lista = estDao.obtener();
+        } catch (Exception e) {
+
+            JOptionPane.showMessageDialog(this, e.getMessage(), "error", JOptionPane.ERROR_MESSAGE);
+        }
+        for (Estudiante est : lista) {
+            modelo.addRow(new Object[]{est.getCodigo(), est.getCedula(), est.getNombres(), est.getApellidos(), est.getFechaNacimiento(), est.getFechaInreso(), est.getTelefono(), est.getSexo(), est.getDireccion(), est.getCurso().getCodigo()});
+        }
+        tabla.setModel(modelo);
+    }
+
 }
